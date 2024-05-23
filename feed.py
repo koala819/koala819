2d7f5c4166b8d87aa775b71f51b8cc0692f3ee07
Xavier G <70317658+koala819@users.noreply.github.com>
2024-05-23 21:55:22 +0200

    Update feed.py

--- feed.py
+++ feed.py
@@ -1,7 +1,6 @@
 import logging
 from typing import Any
 import pathlib
-import feedparser
 import requests
 import re
 from datetime import datetime
@@ -11,46 +10,42 @@ DEFAULT_DATE_FORMAT = "%Y-%m-%d"
 
 root = pathlib.Path(__file__).parent.resolve()
 
-
-def fetch_feed(url: str) -> list[dict[str, str]]:
+def fetch_posts(url: str) -> list[dict[str, str]]:
     try:
         response = requests.get(url)
         response.raise_for_status()
-        feed = feedparser.parse(response.content)
-        if not feed.entries:
-            logging.error("Malformed feed: no entries found")
+        posts = response.json()
+        if not posts:
+            logging.error("Malformed response: no posts found")
             return []
     except requests.exceptions.RequestException as e:
-        logging.error(f"Error fetching the feed: {e}")
+        logging.error(f"Error fetching the posts: {e}")
         return []
     except Exception as e:
-        logging.error(f"Error processing the feed: {e}")
+        logging.error(f"Error processing the posts: {e}")
         return []
 
     return [
         {
-            "title": entry.title,
-            "url": entry.link,
-            "date": format_entry_date(entry),
+            "title": post["title"],
+            "url": post["url"],
+            "date": format_post_date(post),
         }
-        for entry in feed.entries[:DEFAULT_N]
+        for post in posts[:DEFAULT_N]
     ]
 
-
-def format_feed_entry(entry: dict[str, str]) -> str:
+def format_post_entry(entry: dict[str, str]) -> str:
     title = entry.get("title", "No Title")
     link = entry.get("url", "")
     date = entry.get("date", "")
 
     return f"[{title}]({link}) - {date}"
 
-
-def format_entry_date(entry: Any, date_format: str = DEFAULT_DATE_FORMAT) -> str:
-    if hasattr(entry, "published_parsed"):
-        published_time = datetime(*entry.published_parsed[:6])
+def format_post_date(post: Any, date_format: str = DEFAULT_DATE_FORMAT) -> str:
+    if "publishedDate" in post:
+        published_time = datetime.fromisoformat(post["publishedDate"])
         return published_time.strftime(date_format)
     return ""
-
 
 def replace_chunk(content, marker, chunk, inline=False):
     pattern = f"<!-- {marker} start -->.*<!-- {marker} end -->"
@@ -65,9 +60,9 @@ def replace_chunk(content, marker, chunk, inline=False):
 if __name__ == "__main__":
     readme = root / "README.md"
     url = "https://www.dix31.com/api/atoms"
-    feeds = fetch_feed(url)
-    feeds_md = "\n\n".join([format_feed_entry(feed) for feed in feeds])
+    posts = fetch_posts(url)
+    posts_md = "\n\n".join([format_post_entry(post) for post in posts])
     readme_contents = readme.read_text()
-    rewritten = replace_chunk(readme_contents, "blog", feeds_md)
+    rewritten = replace_chunk(readme_contents, "blog", posts_md)
     readme.write_text(rewritten)
-    print(feeds_md)
+    print(posts_md)
